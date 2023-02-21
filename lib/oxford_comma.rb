bb976cbfc83492e2aee93c9e1ea4bb4717eb1ef6
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-02-21 23:21:43 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
-end
+        array
+         case
+         when array.length == 1 
+           return  array.join()
+     
+         when array.length == 2
+             return  array.join(" and ")
+     
+         when array.length >= 2
+             last = array.pop  
+                 array.push("and ")
+                 wholarray = array.join(', ')
+                 wholarray = wholarray + last
+                 return wholarray
+         end
+     end
